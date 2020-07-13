a2933824192ddaf1d55745b87f241fe0ebce4a26
hadeelgamal <43421729+hadeelgamal@users.noreply.github.com>
2020-07-13 12:00:56 +0200

    API request
    adjusting the base url and the key app id with the right syntax. Also adding country codes dropdown to the html as part of the parameters for the call back function

--- website/app.js
+++ website/app.js
@@ -1,6 +1,37 @@
 /* Global Variables */
 
-const postData = async ( url = '/', data = {})=>{
+// Create API key
+let baseURL = 'https://api.openweathermap.org/data/2.5/weather?';
+let apiKey = '&appid=161e1b851a819821e5bc1b43cef53eb5';	
+
+
+
+document.getElementById('generate').addEventListener('click', performAction);
+function performAction(e){
+const zipCode =  'zip='+document.getElementById('zip').value;
+const countryCode = document.getElementById('CC').value;
+getDay(baseURL, zipCode, countryCode, apiKey)
+
+}
+
+// async function that uses GET 
+const getDay = async (baseURL, zipCode, countryCode, apiKey) =>{ 
+  const request = await fetch(baseURL+zipCode+','+countryCode+apiKey);
+  try {
+  // Transform into JSON
+  const allData = await request.json()
+  }
+  catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+  }
+}
+
+
+
+
+// first post route
+const postData = async ( url = 'localhost:8000/datapost', data = {})=>{
     console.log(data);
       const response = await fetch(url, {
       method: 'POST', 
@@ -21,7 +52,7 @@ const postData = async ( url = '/', data = {})=>{
       }
   }
 
-const GetData = async ( url = '/', data = {})=>{
+const GetData = async ( url = 'localhost:8000/dataget', data = {})=>{
     console.log(data);
       const response = await fetch(url, {
       method: 'GET', 
@@ -45,3 +76,4 @@ const GetData = async ( url = '/', data = {})=>{
   // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
